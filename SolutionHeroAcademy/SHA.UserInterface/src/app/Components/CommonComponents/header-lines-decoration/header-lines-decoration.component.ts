44c9b9b39c39360873ed1716708309a0666c743f
Joao Mantovani <84257811+0mochileiro@users.noreply.github.com>
2024-07-09 19:24:31 -0300

    Update "SHA.UserInterface" - Creating new component "HeaderLinesDecoration"
    Creating new component "HeaderLinesDecoration" to implement in "app.component".

---
+++ SolutionHeroAcademy/SHA.UserInterface/src/app/Components/CommonComponents/header-lines-decoration/header-lines-decoration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-lines-decoration',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-lines-decoration.component.html',
+  styleUrl: './header-lines-decoration.component.css'
+})
+export class HeaderLinesDecorationComponent {
+
+}
