bcf3d1469bee501b85da782632a4eaad8fe99019
Joao Mantovani <84257811+0mochileiro@users.noreply.github.com>
2024-07-09 19:37:47 -0300

    Update in "SHA.UserInterface" - Update in "AppComponent"
    Implementation of the following components: "HeaderLinesDecorationComponent", "NavigationMenuComponent" and "FooterComponent" in the app layout.

--- SolutionHeroAcademy/SHA.UserInterface/src/app/app.component.ts
+++ SolutionHeroAcademy/SHA.UserInterface/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { HeaderLinesDecorationComponent } from './Components/CommonComponents/header-lines-decoration/header-lines-decoration.component';
+import { NavigationMenuComponent } from './Components/CommonComponents/navigation-menu/navigation-menu.component';
+import { FooterComponent } from './Components/CommonComponents/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule, RouterOutlet,
+    NavigationMenuComponent, FooterComponent, HeaderLinesDecorationComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'SHA.UserInterface';
+  title = 'Hero Academy';
 }
