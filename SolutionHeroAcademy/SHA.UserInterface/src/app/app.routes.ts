f6c11112a922507065134d465f6d0147e02e9c74
Joao Mantovani <84257811+0mochileiro@users.noreply.github.com>
2024-07-09 19:45:49 -0300

    Update in "SHA.UserInterface" - Update in "AppRoutes"
    Implement initial application routes.

--- SolutionHeroAcademy/SHA.UserInterface/src/app/app.routes.ts
+++ SolutionHeroAcademy/SHA.UserInterface/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/CommonComponents/home/home.component';
+import { GameComponent} from './Components/GameComponents/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' }, // Note: Handle empty urls.
+    { path: 'home', component: HomeComponent, title: 'Hero Academy - Home' }, // Note: Default route.
+    { path: 'game', component: GameComponent, title: 'Hero Academy - Play' }, // Note: Game
+]
