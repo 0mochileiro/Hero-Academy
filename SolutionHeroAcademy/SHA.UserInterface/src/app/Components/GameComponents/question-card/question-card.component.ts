df4c9385d101458e222c5eff6849cf0c336b9ecb
Joao Mantovani <84257811+0mochileiro@users.noreply.github.com>
2024-07-12 18:58:42 -0300

    Update in "SUA.User Interface" - Update the QuestionCardComponent
    Update the QuestionCardComponent to use the QuestionManagerService and implement load animation for questions.

--- SolutionHeroAcademy/SHA.UserInterface/src/app/Components/GameComponents/question-card/question-card.component.ts
+++ SolutionHeroAcademy/SHA.UserInterface/src/app/Components/GameComponents/question-card/question-card.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'
 
-import { QuestionModel } from '../../../Models/QuestionsModel';
+import { QuestionManagerService } from '../../../Services/question-manager-service';
+import { Question, ResponseGetRandomQuestion } from '../../../Models/QuestionManagerServiceModel';
+import { timeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-question-card',
@@ -13,30 +15,43 @@ import { QuestionModel } from '../../../Models/QuestionsModel';
 })
 
 export class QuestionCardComponent implements OnInit {
-
-  model: QuestionModel | undefined;
+  model: Question | undefined;
   selectedAnswerId: string = '';
 
+  constructor(private service: QuestionManagerService) {
+  }
+
   ngOnInit(): void {
-    this.model = new QuestionModel();
-
-    console.log(this.model);
+    this.getQuestion();
   }
 
-  sendAnswer(): void {
-    this.verifyAnswer(this.selectedAnswerId);
+  getQuestion(): void {
+    // this.service.getRandomQuestion().subscribe((response) => (this.model = response.result)); //Correct
 
-    this.loadNextQuestion();
-
+    setTimeout(() => {
+      this.service.getRandomQuestion().subscribe((response) => (this.model = response.result)); // Simulate server response time
+    }, 5000);
   }
 
-  verifyAnswer(selectedAnswerId: string): void {
-    let selectedAnswer = this.model?.answers.find(a => a.id === selectedAnswerId);
+  checkAnswer(): void {
+    let selectedAnswer = this.model?.answers?.find(a => a.id === this.selectedAnswerId);
 
-    console.log(selectedAnswer);
+    this.displayAlert(selectedAnswer?.correct);
+    this.resetComponentState();
+    this.getQuestion();
   }
 
-  loadNextQuestion() {
+  displayAlert(rightAnswer: boolean | undefined): void {
+    if (rightAnswer) {
+      alert("Correct - Generating new question");
+    }
+    else {
+      alert("Wrong - Generating new question");
+    }
+  }
+
+  resetComponentState(): void {
+    this.model = undefined;
     this.selectedAnswerId = '';
   }
 }
