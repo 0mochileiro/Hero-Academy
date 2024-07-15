95dcc613c822593542c28d045f16b2b41bfe39e8
0mochileiro <84257811+0mochileiro@users.noreply.github.com>
2024-07-14 22:04:18 -0300

    Create "SHA.DevelopmentConsole"
    Create console project to speed up the development process in the developer's local environment.

---
+++ SolutionHeroAcademy/SHA.DevelopmentConsoles/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("This is a development console, used to streamline the development process in the local environment. Therefore, it is not necessary to commit your tests.");
+
+Console.WriteLine("May the force be with you");
