3ce740ef8ad9ca2636158e8ddb260024f98292b5
0mochileiro <84257811+0mochileiro@users.noreply.github.com>
2024-07-20 21:09:08 -0300

    Update in "SHA.UserInterface" - Create HeroManagerService
    Create new service to HeroManagerService to handler with hero requests and update QuestionManagerService.

---
+++ SolutionHeroAcademy/SHA.UserInterface/src/app/Models/Responses/HeroManagerServiceResponse.ts
@@ -0,0 +1,15 @@
+export interface ResponseGetRandomHero {
+
+    ok: boolean;
+    message: string;
+    result: Hero;
+}
+
+export interface Hero {
+    id: string;
+    name: string;
+    skill: string;
+    history: string;
+    health: number;
+    attack: number;
+}
--- SolutionHeroAcademy/SHA.UserInterface/src/app/Services/hero-manager.service.ts
+++ SolutionHeroAcademy/SHA.UserInterface/src/app/Services/hero-manager.service.ts
@@ -1,26 +1,27 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../environments/environment';
-import { ResponseGetRandomQuestion } from '../Models/QuestionManagerServiceModel';
+import { ResponseGetRandomHero } from '../Models/Responses/HeroManagerServiceResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class QuestionManagerService {
+
+export class HeroManagerService {
   private readonly uri = environment.SERVICE_URI;
 
   constructor(private httpClient: HttpClient) {
   }
 
-  public getRandomQuestion(): Observable<ResponseGetRandomQuestion> {
+  public getRandomHero(): Observable<ResponseGetRandomHero> {
     let headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'ngrok-skip-browser-warning': 'true'
     });
 
-    return this.httpClient.get<ResponseGetRandomQuestion>(`${this.uri}QuestionManager/GetRandomQuestion`, {headers});
+    return this.httpClient.get<ResponseGetRandomHero>(`${this.uri}HeroManager/GetRandomHero`, { headers });
   }
 }
